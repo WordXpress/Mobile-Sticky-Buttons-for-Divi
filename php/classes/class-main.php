68610942aae76af2b4b5986a5aa9c55a79f8c70d
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-03-31 09:17:42 +0200

    Moving all admin functions to the admin class Movving what seems to be template files to a template directory

--- php/classes/class-main.php
+++ php/classes/class-main.php
@@ -1,108 +1,23 @@
 <?php
 
 /**
- * @todo refactor this to be part of the Main class
+ * Class LCBMain
  */
-
-/* Adding Latest jQuery from Wordpress */
-function lead_call_button_latest_jquery() {
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'lead_call_button_movement_script', plugins_url( '/js/movement.js', __FILE__ ) );
-	wp_enqueue_script( 'lead_call_button_script', plugins_url( '/js/script.js', __FILE__ ) );
-}
-
-add_action( 'init', 'lead_call_button_latest_jquery' );
-
-function lead_call_button_admin_init() {
-	wp_register_script( 'lead_call_button_script', plugins_url( '/js/script.js', __FILE__ ) );
-	wp_enqueue_style( 'lead_call_button_style', plugins_url( '/css/backend-styles.css', __FILE__ ) );
-}
-
-add_action( 'admin_init', 'lead_call_button_admin_init' );
-
-/* LCB Hide Meta Box */
-function lead_call_buttons_options_get_meta( $value ) {
-	global $post;
-	$field = get_post_meta( $post->ID, $value, true );
-	if ( ! empty( $field ) ) {
-		return is_array( $field ) ? stripslashes_deep( $field ) : stripslashes( wp_kses_decode_entities( $field ) );
-	} else {
-		return false;
-	}
-}
-
-function lead_call_buttons_options_add_meta_box() {
-	add_meta_box(
-		'lead_call_buttons_options-lead-call-buttons-options',
-		__( 'Lead Call Buttons Options', 'lead_call_buttons_options' ),
-		'lead_call_buttons_options_html',
-		'post',
-		'side',
-		'low'
-	);
-	add_meta_box(
-		'lead_call_buttons_options-lead-call-buttons-options',
-		__( 'Lead Call Buttons Options', 'lead_call_buttons_options' ),
-		'lead_call_buttons_options_html',
-		'page',
-		'side',
-		'low'
-	);
-}
-
-add_action( 'add_meta_boxes', 'lead_call_buttons_options_add_meta_box' );
-
-function lead_call_buttons_options_html( $post ) {
-	wp_nonce_field( '_lead_call_buttons_options_nonce', 'lead_call_buttons_options_nonce' ); ?>
-	<p>
-		<input type="checkbox" name="lead_call_buttons_options_hide_lead_call_buttons"
-			   id="lead_call_buttons_options_hide_lead_call_buttons"
-			   value="hide-lead-call-buttons" <?php echo ( lead_call_buttons_options_get_meta( 'lead_call_buttons_options_hide_lead_call_buttons' ) === 'hide-lead-call-buttons' ) ? 'checked' : ''; ?>>
-		<label for="lead_call_buttons_options_hide_lead_call_buttons"><strong><?php _e( 'Hide Lead Call Buttons', 'lead_call_buttons_options' ); ?></strong></label>
-	</p>
-	<?php
-}
-
-function lead_call_buttons_options_save( $post_id ) {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-	if ( ! isset( $_POST['lead_call_buttons_options_nonce'] ) || ! wp_verify_nonce( $_POST['lead_call_buttons_options_nonce'], '_lead_call_buttons_options_nonce' ) ) {
-		return;
-	}
-	if ( ! current_user_can( 'edit_post', $post_id ) ) {
-		return;
-	}
-
-	if ( isset( $_POST['lead_call_buttons_options_hide_lead_call_buttons'] ) ) {
-		update_post_meta( $post_id, 'lead_call_buttons_options_hide_lead_call_buttons', esc_attr( $_POST['lead_call_buttons_options_hide_lead_call_buttons'] ) );
-	} else {
-		update_post_meta( $post_id, 'lead_call_buttons_options_hide_lead_call_buttons', null );
-	}
-}
-
-add_action( 'save_post', 'lead_call_buttons_options_save' );
-
-/**
- * @todo refactor this to be part of the Main class
- */
-
-/* Main Class */
-
 class LCBMain {
 
 	private $plugin_path;
 	private $plugin_url;
-	private $frame_work;
+
+	private $admin;
 	private $LCB; //phpcs:ignore
 
 	function __construct() {
 
 		$this->plugin_path = LEAD_CALL_BUTTON_PLUGIN_PATH;
 		$this->plugin_url  = LEAD_CALL_BUTTON_PLUGIN_DIR;
-		$this->frame_work  = 'lead-call-button-frame';
 
-		$this->LCB = new LCB_setting( $this->plugin_path . 'settings/lead-call-button-settings-general.php' );
+		$this->LCB = new LCB_setting( $this->plugin_path . 'php/templates/settings/lead-call-button-settings-general.php' );
+		$this->admin = new LCB_Admin();
 
 		// @todo remove this on clean up
 		//wp_enqueue_style('lead_call_button_css', LEAD_CALL_BUTTON_HOOK.'css/plugin-main.css');
@@ -230,6 +145,6 @@ class LCBMain {
 	 * Includes the actual lead buttons functionality
 	 */
 	public function lead_call_button_active_hook() {
-		include_once( 'lead-call-buttons.php' );
+		include_once( $this->plugin_path . 'php/templates/lead-call-buttons.php' );
 	}
 }
